482ae12a3f49ccc93f7d1911118f44d2e28c39f0
diwakar8004 <diwakargond923@gmail.com>
2024-11-19 10:51:18 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,7 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function () {
+    alert("Account creation successfull. Use your username which is send at your email. Thank Your");
+});
+
+let setPass = document.getElementById("#setPass");
